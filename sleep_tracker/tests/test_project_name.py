a666df7abcadce7fa506bc543bc43f7cca418d2e
Joeskido Ama <joeskido@Joeskidos-MacBook-Pro.local>
2023-08-31 16:53:00 -0400

    add file

--- sleep_tracker/tests/test_project_name.py
+++ sleep_tracker/tests/test_project_name.py
@@ -4,4 +4,17 @@ import unittest
 class Testsleep_tracker(unittest.TestCase):
 
     def test_failure(self):
-        self.assertFalse(True)
+        # self.assertFalse(True)
+        self.assertTrue(True)
+
+
+
+
+
+
+
+
+
+
+
+# AssertionError: True is not false
